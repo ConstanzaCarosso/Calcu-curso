81de2bd7af2ffeb373edf735df939cda53cb5591
Constanza <constanzacarosso@gmail.com>
2020-01-29 16:48:02 -0300

    curso

---
+++ programa.js
@@ -0,0 +1,32 @@
+function funcionsumar ( parametro ){
+    var a = prompt("Ingrese el valor de a");
+    if (confirm);
+    var b = prompt("Ingrese el valor de b");
+    if (confirm);
+    var resultado = a + b;
+    prompt ("La suma de a + b es: " + resultado);
+}
+function funcionrestar ( parametro ){
+    var a = prompt("Ingrese el valor de a");
+    if (confirm);
+    var b = prompt("Ingrese el valor de b");
+    if (confirm);
+    var resultado = (a - b);
+    prompt ("La suma de a - b es: " + resultado);
+}
+function funcionmultiplicar ( parametro ){
+    var a = prompt("Ingrese el valor de a");
+    if (confirm);
+    var b = prompt("Ingrese el valor de b");
+    if (confirm);
+    var resultado = (a * b);
+    prompt ("La suma de a * b es: " + resultado);
+}
+function funciondividir ( parametro ){
+    var a = prompt("Ingrese el valor de a");
+    if (confirm);
+    var b = prompt("Ingrese el valor de b");
+    if (confirm);
+    var resultado = (a / b);
+    prompt ("La suma de a / b es: " + resultado);
+}
